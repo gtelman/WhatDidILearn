8cd74ad4692dc69ac03fd94c028f275039d0ae4b
Gabrielle Telman <gabrielletelman@gmail.com>
2020-11-03 20:57:02 -0300

    adding room

--- app/src/main/java/com/wcc/whatdidilearn/entities/ItemLearned.kt
+++ app/src/main/java/com/wcc/whatdidilearn/entities/ItemLearned.kt
@@ -1,7 +1,19 @@
 package com.wcc.whatdidilearn.entities
 
-class ItemLearned(
+import androidx.room.Entity
+
+@Entity(tableName= "learned_Item")
+data class ItemLearned(
+    @ColumnInfo(name = "item_title")
     val title: String,
+
+    @ColumnInfo(name = "item_description")
     val description: String,
-    val understandingLevel: UnderstandingLevel
+
+    @ColumnInfo(name = "item_understanding_level")
+    val understandingLevel: UnderstandingLevel,
+
+    @PrimaryKey(autogenerate = true)
+    @ColumnInfo(name = "item_id")
+    val id:Int = 0,
 )
--- app/src/main/java/com/wcc/whatdidilearn/view/MainActivity.kt
+++ app/src/main/java/com/wcc/whatdidilearn/view/MainActivity.kt
@@ -1,18 +1,20 @@
 package com.wcc.whatdidilearn.view
 
-import androidx.appcompat.app.AppCompatActivity
+import  androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.RecyclerView
 import com.wcc.whatdidilearn.R
 import com.wcc.whatdidilearn.data.DatabaseItems
+import com.wcc.whatdidilearn.databinding.ActivityMainBinding
 import com.wcc.whatdidilearn.entities.ItemLearned
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val recyclerView = findViewById<RecyclerView>(R.id.items_recycler_view)
+        val recyclerView = binding.itemsRecyclerView
         val adapter = LearnedItemsAdapter()
 
         recyclerView.adapter = adapter
